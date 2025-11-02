057276a6904127a4251a6ce3c3524213aa53b2b4
Wai Phyo Aung <waiphyoag.cs34@gmail.com>
2025-11-03 00:30:30 +0700

    error deploying to coolify,node and npm version outdated

--- nexuslab/app/page.tsx
+++ nexuslab/app/page.tsx
@@ -1,5 +1,34 @@
-import { redirect } from 'next/navigation'
+'use client'
 
-export default function Home() {
-  redirect('/nexus')
+import AboutSection from '@/components/layout/AboutSection'
+import ContactSection from '@/components/layout/ContactSection'
+import Footer from '@/components/layout/Footer'
+import HeroSection from '@/components/layout/HeroSection'
+import Navigation from '@/components/layout/Navigation'
+import ProjectsSection from '@/components/layout/ProjectsSection'
+import ServicesSection from '@/components/layout/ServicesSection'
+import { useGSAPAnimations } from '@/hooks'
+
+export default function NexusLanding() {
+  const { main, scrollToSection, navRef, navContainerRef, logoRef, menuRef } = useGSAPAnimations()
+
+  return (
+    <div id="smooth-wrapper" ref={main}>
+      <div id="smooth-content">
+        <Navigation 
+          scrollToSection={scrollToSection}
+          navRef={navRef}
+          navContainerRef={navContainerRef}
+          logoRef={logoRef}
+          menuRef={menuRef}
+        />
+        <HeroSection scrollToSection={scrollToSection} />
+        <AboutSection />
+        <ServicesSection />
+        <ProjectsSection />
+        <ContactSection />
+        <Footer />
+      </div>
+    </div>
+  )
 }
